c56c393c28dad7dad1b321897b2593f79cce8c37
sreyniet.loeurn <sreyniet.loeurn@PNC.LAN>
2020-01-04 20:10:03 +0700

    homework1

---
+++ resources/views/pages/news.blade.php
@@ -0,0 +1,5 @@
+<h1>News</h1>
+<p>
+Find all the latest news, stories and unique insights from Passerelles numériques here!
+</p>
+
---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/our_action', function () {
+    return view('pages.our_action');
+});
+
+Route::get('/about_us', function () {
+    return view('pages.about_us');
+});
+
+Route::get('/news', function () {
+    return view('pages.news');
+});
+
+Route::get('/join_us', function () {
+    return view('pages.join_us');
+});
+
+Route::get('/contact_us', function () {
+    return view('pages.contact_us');
+});
